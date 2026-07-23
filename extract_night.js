6010e6ab6ab3a1a198b248b5280375488b285b57
PhoorinS <91134913+PhoorinS@users.noreply.github.com>
2026-07-23 22:41:31 +0700

    Chore: Clean up unused CSS and legacy refactor scripts

--- extract_night.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/games/Werewolf.tsx', 'utf8');
-
-const nightStart = content.indexOf("{phase === 'night' && (");
-const nightEnd = content.indexOf("{phase === 'day' && (");
-const nightRender = content.substring(nightStart, nightEnd);
-
-fs.writeFileSync('src/games/werewolf/night_render.txt', nightRender);
